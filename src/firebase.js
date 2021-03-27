8c2a11bab86ce5c2f5b8a7aa7395c9288a898a00
rishya-prabakaran <60148918+rishya-prabakaran@users.noreply.github.com>
2021-03-27 11:30:50 +0530

    Updated changes

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCz5KaRFfyMe5OyLfVyUDlCh6-E8KZMniY",
-    authDomain: "clone-d82df.firebaseapp.com",
-    projectId: "clone-d82df",
-    storageBucket: "clone-d82df.appspot.com",
-    messagingSenderId: "559072720568",
-    appId: "1:559072720568:web:a7e5ac4d6caa664ee4a64d",
-    measurementId: "G-MLD8TV5XWG"
-  };
